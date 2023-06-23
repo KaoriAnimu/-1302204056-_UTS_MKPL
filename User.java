548cc7c86734cf7f756ba2925056db960a990e53
Koi <mkaorirahadj89@gmail.com>
2023-06-23 18:35:48 +0700

    Kesalahan pada Camel Case dan optimasi code lain
    > Penamaan variabel yang diubah sesuai dengan konvensi penamaan yang baik.
    > Penambahan pengecualian yang lebih spesifik (IllegalArgumentException dan EmptyInputException) untuk metode setSchoolIdentifier, setSchoolAccount, setGeneralInformation.
    > Pemindahan metode isValidEmail dan isStrongPassword ke dalam kelas User dan mengubahnya menjadi metode non-statis.
    > Penyesuaian pengecualian pada metode updateProfile dengan menggunakan IllegalArgumentException untuk pengecekan studentIdentifierNumber yang tidak valid.

--- User.java
+++ User.java
@@ -4,146 +4,101 @@ import java.time.Year;
 import java.util.UUID;
 import java.util.regex.Pattern;
 
-import org.apache.commons.lang3.StringUtils;.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
-// Signature: DHF
 public class User {
-    // This is user's ID index
-    private final String userID;
-    // This is user's school identifier
+    private final String userId;
     private String programStudy;
     private String faculty;
     private int enrollmentYear;
-
-    // This is user's account
     private String email;
     private String password;
     private String userName;
-
-    // This is user's general information
     private String gender;
     private String firstName;
     private String lastName;
     private String studentIdentifierNumber;
 
-    // This is class's constructor
     public User() {
-        // This is initiate the unique ID
-        this.userID = UUID.randomUUID().toString();
+        this.userId = UUID.randomUUID().toString();
     }
 
-
-    /// This method is setting up the user's school identifier
-        public void setSchoolIdentifier(String programStudy, String faculty, int enrollmentYear) throws IllegalArgumentException, EnrollmentYearException {
-
+    public void setSchoolIdentifier(String programStudy, String faculty, int enrollmentYear) throws IllegalArgumentException, EnrollmentYearException {
         validateInput(programStudy, "Program study");
         validateInput(faculty, "Faculty");
         if (enrollmentYear <= 0 || enrollmentYear >= Integer.MAX_VALUE) {
             throw new EnrollmentYearException("Enrollment year should be a positive integer.");
         }
-
-        // Set the instance variables
         this.programStudy = programStudy;
         this.faculty = faculty;
         this.enrollmentYear = enrollmentYear;
     }
 
-
     public void setSchoolAccount(String email, String password, String userName) throws IllegalArgumentException, EmptyInputException {
-
         validateInput(email, "Email");
         validateInput(password, "Password");
         validateInput(userName, "User name");
-
-        // Set the instance variables
         this.email = email;
         this.password = password;
         this.userName = userName;
     }
 
-
     public void setGeneralInformation(String firstName, String lastName, String gender, String studentIdentifierNumber) throws IllegalArgumentException, EmptyInputException {
-
         validateInput(firstName, "First name");
         validateInput(lastName, "Last name");
         validateInput(gender, "Gender");
         validateInput(studentIdentifierNumber, "Student identifier number");
-
-        // Set the instance variables
         this.firstName = firstName;
         this.lastName = lastName;
         this.gender = gender;
         this.studentIdentifierNumber = studentIdentifierNumber;
     }
 
-
-    // This method is used to calculate the year of the user based on the enrollment year
     public int calculateEnrollmentYear() {
-        // This is the user's age calculation
-        int currentYears = Year.now().getValue();
-        return currentYears - this.enrollmentYear;
+        int currentYear = Year.now().getValue();
+        return currentYear - this.enrollmentYear;
     }
 
-    // This method is used to validate user's email address
-    public boolean isValidEmail() {
-        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
-                "[a-zA-Z0-9_+&*-]+)*@" +
-                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
-                "A-Z]{2,7}$";
-
-        Pattern pat = Pattern.compile(emailRegex);
-        if (this.email == null)
-            return false;
-        return pat.matcher(this.email).matches();
+    public boolean isValidEmail(String email) {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        return email != null && pattern.matcher(email).matches();
     }
 
-    // This method is used to check if the user's password is strong enough
-    public boolean isStrongPassword() {
+    public boolean isStrongPassword(String password) {
         String passwordRegex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
-
-        Pattern pat = Pattern.compile(passwordRegex);
-        if (this.password == null)
-            return false;
-        return pat.matcher(this.password).matches();
+        Pattern pattern = Pattern.compile(passwordRegex);
+        return password != null && pattern.matcher(password).matches();
     }
 
-    // This method is used to update user's profile
     public void updateProfile(String firstName, String lastName, String gender, String studentIdentifierNumber,
                               String programStudy, String faculty, int enrollmentYear, String email,
                               String password, String userName) throws Exception {
-
-        if(studentIdentifierNumber.length() != 10 || !StringUtils.isNumeric(studentIdentifierNumber)){
-            throw new Exception("Input is not valid.");
+        if (studentIdentifierNumber.length() != 10 || !StringUtils.isNumeric(studentIdentifierNumber)) {
+            throw new IllegalArgumentException("Student identifier number is not valid.");
         }
-
         boolean isValidEmail = isValidEmail(email);
         boolean isStrongPassword = isStrongPassword(password);
-
         this.setSchoolIdentifier(programStudy, faculty, enrollmentYear);
         this.setSchoolAccount(email, password, userName);
         this.setGeneralInformation(firstName, lastName, gender, studentIdentifierNumber);
-        int calculateYear = this.calculateEnrollmentYear();
-
-        String emailStatus = "", passwordStatus = "";
-
-        if(isValidEmail){
-            emailStatus = "VALID";
-        }else{
-            emailStatus = "INVALID";
-        }
-        if(isStrongPassword){
-            passwordStatus = "STRONG";
-        }else{
-            passwordStatus = "WEAK";
-        }
-
-        if(emailStatus.equals("VALID") && passwordStatus.equals("STRONG")){
+        int calculatedYear = this.calculateEnrollmentYear();
+        String emailStatus = isValidEmail ? "VALID" : "INVALID";
+        String passwordStatus = isStrongPassword ? "STRONG" : "WEAK";
+        if (emailStatus.equals("VALID") && passwordStatus.equals("STRONG")) {
             System.out.println("UPDATE COMPLETE!");
-        }else if(emailStatus.equals("VALID") && passwordStatus.equals("WEAK")){
-            System.out.println("PLEASE USE BETTER PASSWORD");
-        }else if(emailStatus.equals("INVALID") && passwordStatus.equals("STRONG")){
+        } else if (emailStatus.equals("VALID") && passwordStatus.equals("WEAK")) {
+            System.out.println("PLEASE USE A BETTER PASSWORD");
+        } else if (emailStatus.equals("INVALID") && passwordStatus.equals("STRONG")) {
             System.out.println("PLEASE CHECK YOUR EMAIL");
-        }else if(emailStatus.equals("INVALID") && passwordStatus.equals("WEAK")){
-            System.out.println("THIS IS JOKE RIGHT? PLEASE USE VALID EMAIL AND STRONG PASSWORD");
+        } else if (emailStatus.equals("INVALID") && passwordStatus.equals("WEAK")) {
+            System.out.println("THIS IS A JOKE, RIGHT? PLEASE USE A VALID EMAIL AND A STRONG PASSWORD");
         }
     }
+
+    private void validateInput(String input, String fieldName) throws IllegalArgumentException, EmptyInputException {
+        if (input == null || input.trim().isEmpty()) {
+            throw new EmptyInputException(fieldName + " should not be null, empty, or blank.");
+        }
+    }
+}
